cc35062d80fb9a157928e8088f574438fcdad6cf
Hasan Kara <hasan.kara@fhnw.ch>
2015-08-14 13:58:44 +0200

    Init

---
+++ src/main/java/ch/fhnw/eval/VaadinWithSpringBootApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinWithSpringBootApplication.class, args);
+    }
+}
